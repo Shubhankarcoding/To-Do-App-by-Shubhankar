522d4e3f635c2bdabf4134bdd9070cac1e1397b5
Shubhankarcoding <77837780+Shubhankarcoding@users.noreply.github.com>
2021-03-20 14:56:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDoApp</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div>
+		<h2>My To Do List</h2>
+		<input type="text" id="myInput" placeholder="Title...">
+		<span onclick="newElement()" class="addBtn">Add</span>
+	</div>
+
+
+
+</div>
+</body>
+</html>
